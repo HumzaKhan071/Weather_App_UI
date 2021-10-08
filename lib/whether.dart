bc9ea6769bd2a65210d30fae0212728beedb1c1c
Humza Khan <Humzakhan071@gmail.com>
2021-10-09 00:07:23 +0500

    Weather_App_UI

---
+++ lib/whether.dart
@@ -0,0 +1,18 @@
+
+class Whether{
+  int id;
+  String day;
+  String temp;
+  String whether;
+  String imaDay;
+  String imgNight;
+
+  Whether({
+    this.id,
+    this.day,
+    this.temp,
+    this.whether,
+    this.imaDay,
+    this.imgNight
+});
+}
